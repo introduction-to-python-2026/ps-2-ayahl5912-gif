fef1867d0a152f15d035683ea0e45abcc0c96a1d
ayahl5912-gif <ayahl5912@gmail.com>
2025-11-01 12:46:28 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,32 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
-
+    if (num1 >= num2) and (num1 >= num3):
+      return(num1)
+    elif (num2 > num3):
+      return(num2)
+    else:
+      return(num3) # Replace 'pass' with code
+num1 = 1
+num2 = 3
+num3 = 3
+max_num = find_max_number(num1, num2, num3)
+print("The maximum number is:", max_num)
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
-
+    mean = (num1+num2+num3)/3  
+    return mean # Replace 'pass' with code
+num1 = 1
+num2 = 3
+num3 = 2
+mean_value = find_mean(num1, num2, num3)
+print("The mean is:", mean_value)
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
-
+    std = ((((num1-mean)**2)+((num2-mean)**2)+((num3-mean)**2))/3)**0.5
+    return std
+    return mean  # Replace 'pass' with code
+num1 = 1
+num2 = 3
+num3 = 2
+mean = find_mean(num1,num2,num3)
+std1 = find_mean_std(num1, num2, num3)
+print("The mean is",mean)
+print("The standard deviation is", std1)
